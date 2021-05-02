73b7acad7b7efb72e43d8087ead37b8b4cf85ab4
Amal-p <amalpvatayam67@gmail.com>
2021-05-02 15:12:02 +0530

    Add new card

--- New chalange/javascript.js
+++ New chalange/javascript.js
@@ -4,8 +4,12 @@ $(document).ready(function(){
 
     if (content.indexOf("<script>") >= 0){
       alert("CTF{l2-xsS-doNe}")
+      $('#cardti').text("Cross Site Scripting (XSS)")
+      $('#des').text("Cross-Site Scripting (XSS) attacks are a type of injection, in which malicious scripts are injected into otherwise benign and trusted websites.CLICK TO KNOW MORE.....")
+      $('#link').attr("href", "https://youtu.be/HGaFCcWM57U");
     }else {
-      $('#hedding').text(content)
+      $('#cardti').text(content)
+      $('#des').text("Keep try, You will get")
     }
   })
 })
